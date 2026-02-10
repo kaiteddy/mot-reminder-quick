ec55f51fddc35cda1c2b46e84244c752dc9f6c5a
Eli Motors <info@elimotors.co.uk>
2026-02-10 14:56:24 +0000

    chore: cleanup test script

--- test_py_path.js
+++
@@ -1,5 +0,0 @@
-import { spawnSync } from 'child_process';
-const res = spawnSync('/usr/bin/python3', ['--version'], { encoding: 'utf-8', shell: true });
-console.log('STDOUT:', res.stdout);
-console.log('STDERR:', res.stderr);
-console.log('STATUS:', res.status);
